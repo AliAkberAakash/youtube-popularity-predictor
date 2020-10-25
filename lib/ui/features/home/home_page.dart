9d0731d0d80dea745b51e4bbbb8a240888e7c0c9
aliakberaakash <ali852609@gmail.com>
2020-10-25 11:48:18 +0600

    adde Homepgage

--- lib/ui/features/home/home_page.dart
+++ lib/ui/features/home/home_page.dart
@@ -5,6 +5,7 @@ import 'package:youtube_popularity_predictor/core/pages/loading_pge.dart';
 import 'package:youtube_popularity_predictor/data/models/message_response.dart';
 import 'package:youtube_popularity_predictor/data/repositories/repository.dart';
 import 'package:youtube_popularity_predictor/di/dependency_injection.dart';
+import 'package:youtube_popularity_predictor/utils/my_colors.dart';
 import 'package:youtube_popularity_predictor/utils/string_formatter.dart';
 
 import 'home_bloc.dart';
@@ -17,7 +18,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  MessageResponse _response;
 
   @override
   void initState() {
@@ -32,32 +32,40 @@ class _HomePageState extends State<HomePage> {
           "Home"
         ),
       ),
-      body: BlocProvider(
-        create: (context) => HomeBloc(repository: locator<Repository>())
-        ..add(GetMessageEvent()), //load data at the start
-        child: Builder(
-          builder: (ctxB){
-            print("Event was loaded");
-            return BlocBuilder<HomeBloc, HomeState>(
-              builder: (ctx, state){
-                if(state is HomeLoadingState){
-                  return LoadingPage();
-                }else if(state is HomeLoadedState) {
-                  if(state.response!=null){
-                    _response = state.response; //in case we need the response
-                    if(state.response.success){
-                      return Center(
-                        child: Text(
-                          checkNull(_response?.message),
-                        ),
-                      );
-                    }
-                  }
-                }
-                return EmptyPage(message: "Failed to load data from the server",);
-              },
-            );
-          },
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: new InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: primary, width: 2.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color:primary ,width: 2.0),
+                  ),
+                  hintText: 'Enter youtube video link or id',
+                ),
+              ),
+
+              MaterialButton(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5.0))),
+                elevation: 5.0,
+                minWidth: 200.0,
+                height: 35,
+                color: primary,
+                child: new Text('Find',
+                    style: new TextStyle(fontSize: 16.0, color: Colors.white)),
+                onPressed: () {
+                  setState(() {
+
+                  });
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
